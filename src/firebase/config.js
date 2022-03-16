2d6c7b54342ac325920d7099217da874ecb361e1
men1164 <41180013+men1164@users.noreply.github.com>
2022-03-16 11:01:42 +0700

    init firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, serverTimestamp } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjosnJ4kkRBmTU31csV9XMGQ8LmGeN8FE",
+  authDomain: "the-dojo-f5a8f.firebaseapp.com",
+  projectId: "the-dojo-f5a8f",
+  storageBucket: "the-dojo-f5a8f.appspot.com",
+  messagingSenderId: "739495246093",
+  appId: "1:739495246093:web:d1c3c5adaad14e92441844"
+};
+
+// init firebase app
+const firebase = initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = getFirestore(firebase)
+const projectAuth = getAuth(firebase)
+
+const timestamp = serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
