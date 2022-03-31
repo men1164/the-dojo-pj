6711d2a28219afe980d0c1333a655af4611c3d6e
men1164 <41180013+men1164@users.noreply.github.com>
2022-03-31 16:40:42 +0700

    dynamic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import './Navbar.css'
 import Temple from '../assets/temple.svg'
 import { Link } from 'react-router-dom'
 import useLogout from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Navbar() {
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
 
   return (
     <div className="navbar">
@@ -13,16 +15,22 @@ export default function Navbar() {
           <img src={Temple} alt="dojo logo" />
           <span>The Dojo</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-          {isPending && <button className="btn" disabled>Logging out...</button>}
-        </li>
+        {!user &&
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        }
+        {user && (
+          <li>
+            {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+            {isPending && <button className="btn" disabled>Logging out...</button>}
+          </li>
+        )}
       </ul>
     </div>
   )
