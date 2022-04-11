d6b69b4cb4046d35b57ba2df7d324061b1056187
men1164 <41180013+men1164@users.noreply.github.com>
2022-04-11 13:35:32 +0700

    user's status completed

--- src/components/Online.js
+++ src/components/Online.js
@@ -11,6 +11,8 @@ export default function Online() {
       {error && <div className="error">{error}</div>}
       {documents && documents.map(user => (
         <div key={user.docId} className="user-list-item">
+          {user.online && <span className="online-user"></span>}
+          {!user.online && <span className="offline-user"></span>}
           <span>{user.displayName}</span>
           <Avatar src={user.photoURL} />
         </div>
