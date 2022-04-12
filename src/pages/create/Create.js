9424cee8de6ce752ce3f5075c12d0a290ea6cf6b
men1164 <41180013+men1164@users.noreply.github.com>
2022-04-12 13:55:34 +0700

    create project form, installed react-select

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,65 @@
 import './Create.css'
+import { useEffect, useState } from 'react'
+import Select from 'react-select'
+import { useCollection } from '../../hooks/useCollection'
+
+const categories = [
+  { value: "development", label: "Development" },
+  { value: "Design", label: "Design" },
+  { value: "Sales", label: "Sales" },
+  { value: "Marketing", label: "Marketing" },
+]
 
 export default function Create() {
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+  
+  const { documents } = useCollection('users')
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    if(documents) {
+      const options = documents.map(user => {
+        return { value: user, label: user.displayName }
+      })
+      setUsers(options)
+    }
+  }, [documents])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(name, details, dueDate, category.value, assignedUsers)
+  }
+
   return (
-    <div>Create</div>
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project Name:</span>
+          <input type="text" onChange={e => setName(e.target.value)} value={name} required />
+        </label>
+        <label>
+          <span>Project Details:</span>
+          <textarea type="text" onChange={e => setDetails(e.target.value)} value={details} required></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input type="date" onChange={e => setDueDate(e.target.value)} value={dueDate} required />
+        </label>
+        <label>
+          <span>Project Category:</span>
+          <Select onChange={option => setCategory(option)} options={categories} />
+        </label>
+        <label>
+          <span>Assigned to:</span>
+          <Select onChange={option => setAssignedUsers(option)} options={users} isMulti />
+        </label>
+        <button className="btn">Add</button>
+      </form>
+    </div>
   )
 }
